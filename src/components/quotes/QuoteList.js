8f2447c53a4b0284f0aa369b81767ff3ce8c911c
sidmohanty11 <sidmohanty11@gmail.com>
2021-05-11 22:36:54 +0530

    dynamic routing

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,13 +1,39 @@
 import { Fragment } from 'react';
-
+import { useHistory, useLocation } from 'react-router-dom';
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
 
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
+
 const QuoteList = (props) => {
+  const history = useHistory();
+  const location = useLocation();
+
+  const qParams = new URLSearchParams(location.search);
+
+  const isSortAsc = qParams.get('sort') === 'asc';
+
+  const sortedQ = sortQuotes(props.quotes, isSortAsc);
+
+  const sortingHandler = () => {
+    history.push(`${location.pathname}?sort=${(isSortAsc ? 'desc':'asc')}`);
+  };
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={sortingHandler}>Sort {isSortAsc ? 'Descending' : 'Ascending'}</button>
+      </div>
       <ul className={classes.list}>
-        {props.quotes.map((quote) => (
+        {sortedQ.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useParams, Route } from 'react-router-dom';
+import { useParams, Route, Link, useRouteMatch } from 'react-router-dom';
 import Comments from '../components/comments/Comments';
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
 
@@ -9,6 +9,7 @@ const QUOTES = [
 ];
 
 const QuoteDetail = () => {
+    const match = useRouteMatch();
     const params = useParams();
     const quote = QUOTES.find(q => q.id === params.quoteId);
 
@@ -17,8 +18,13 @@ const QuoteDetail = () => {
     }
     return (
         <>
-            <HighlightedQuote text={quote.text} author={quote.author}/>
-            <Route path={`/quotes/${params.quoteId}/comments`}>
+            <HighlightedQuote text={quote.text} author={quote.author} />
+            <Route path={`${match.path}`} exact>
+                <div className='centered'>
+                    <Link to={`${match.url}/comments`} className="btn--flat">Load Comments</Link>
+                </div>
+            </Route>
+            <Route path={`${match.path}/comments`}>
                 <Comments />
             </Route>
         </>
