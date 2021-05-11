0997d530927baf9a446904c4e73902b05b12813c
sidmohanty11 <sidmohanty11@gmail.com>
2021-05-12 00:26:55 +0530

    lazy loading and building

---
+++ build/static/js/5.2306edc0.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-complete-guide"]=this["webpackJsonpreact-complete-guide"]||[]).push([[5],{52:function(e,t,c){"use strict";c.r(t);c(1);var n=c(0);t.default=function(){return Object(n.jsx)("div",{className:"centered",children:"404 Page Not Found!"})}}}]);
+//# sourceMappingURL=5.2306edc0.chunk.js.map
--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+import React, {Suspense} from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import Layout from './components/layout/Layout';
+import LoadingSpinner from './components/UI/LoadingSpinner';
 import AllQuotes from './pages/AllQuotes';
-import NewQuote from './pages/NewQuote';
-import NotFound from './pages/NotFound';
-import QuoteDetail from './pages/QuoteDetail';
+
+const NewQuote = React.lazy(() => import('./pages/NewQuote'));
+const QuoteDetail = React.lazy(() => import('./pages/QuoteDetail'));
+const NotFound = React.lazy(() => import('./pages/NotFound'));
 
 function App() {
   return (
     <div>
       <Layout>
+        <Suspense fallback={<div className='centered'><LoadingSpinner /></div>}>
       <Switch>
         <Route path='/' exact>
           <Redirect to='/quotes' />
@@ -25,7 +29,8 @@ function App() {
           <Route path='*'>
             <NotFound />
           </Route>
-        </Switch>
+          </Switch>
+        </Suspense>
       </Layout>
     </div>
   );
